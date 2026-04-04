e38b2c4ffee233629a900c5ec89799834b016540
S3ID420 <oussamawaid@gmail.com>
2026-04-04 21:52:12 +0200

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="absolute top-0 left-0 right-0 z-50  border-b border-white/5">
+    <nav className="fixed top-0 left-0 right-0 z-50  border-b border-white/5">
       <div className="max-w-7xl mx-auto px-6 h-24 flex items-center justify-between">
         {/* Logo */}
         <a href="#home" className="flex items-center gap-2">
